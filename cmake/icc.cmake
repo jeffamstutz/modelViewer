c5d7cc7b8eca8bba802db719b396a87b0fed326f
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2015-09-18 13:40:46 -0400

    cleanup compiler configuration CMake files

--- cmake/icc.cmake
+++ cmake/icc.cmake
@@ -14,9 +14,6 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
-
-SET(CMAKE_CXX_COMPILER "icpc")
-SET(CMAKE_C_COMPILER "icc")
 SET(CMAKE_CXX_FLAGS "-Wall -fPIC -no-ansi-alias -static-intel -openmp")
 SET(CMAKE_CXX_FLAGS_DEBUG          "-DDEBUG  -g")
 SET(CMAKE_CXX_FLAGS_RELEASE        "-DNDEBUG    -O3 -no-ansi-alias -restrict -fp-model fast -fimf-precision=low -no-prec-div -no-prec-sqrt -fma -no-inline-max-total-size -inline-factor=200 ")
@@ -25,18 +22,5 @@ SET(CMAKE_EXE_LINKER_FLAGS "")
 
 IF (APPLE)
   SET (CMAKE_SHARED_LINKER_FLAGS ${CMAKE_SHARED_LINKER_FLAGS_INIT} -dynamiclib)
-  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.7")
+  SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.7")
 ENDIF (APPLE)
-
-# these flags apply ONLY to how embree is built; the rest of the ospray C++ code is ISA-agnostic
-SET(OSPRAY_ARCH_SSE3    "-xsse3")
-SET(OSPRAY_ARCH_SSSE3   "-xssse3")
-SET(OSPRAY_ARCH_SSE41   "-xsse4.1")
-SET(OSPRAY_ARCH_SSE42   "-xsse4.2")
-SET(OSPRAY_ARCH_SSE     "-xsse4.2")
-SET(OSPRAY_ARCH_AVX     "-xAVX")
-SET(OSPRAY_ARCH_AVX2    "-xCORE-AVX2")
-SET(OSPRAY_ARCH_AVX512  "-xMIC-AVX512")
-
-SET(OSPRAY_COMPILER_SUPPORTS_AVX  TRUE)
-SET(OSPRAY_COMPILER_SUPPORTS_AVX2 TRUE)
